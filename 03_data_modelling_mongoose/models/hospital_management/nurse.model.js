3610ab87ca86c456a514df79ff6508677659b8cb
mukulpadwal <mukulpadwal786@gmail.com>
2024-02-15 14:28:59 +0530

    ADD : nurse-details

--- 03_data_modelling_mongoose/models/hospital_management/nurse.model.js
+++ 03_data_modelling_mongoose/models/hospital_management/nurse.model.js
@@ -1,5 +1,33 @@
 import mongoose from "mongoose";
 
-const nurseSchema = new mongoose.Schema({}, { timestamps: true });
+const nurseSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    worksUnderDoctor: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Doctor",
+        },
+      ],
+    },
+    worksInHospitals: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Hospital",
+        },
+      ],
+    },
+  },
+  { timestamps: true }
+);
 
 export const Nurse = mongoose.model("Nurse", nurseSchema);
