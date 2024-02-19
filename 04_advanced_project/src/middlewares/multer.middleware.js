edaae145d4c64aa664107033d18e38c9877f03f3
mukulpadwal <mukulpadwal786@gmail.com>
2024-02-19 13:57:21 +0530

    ADD : file-upload-logic-multer-and-cloudinary

---
+++ 04_advanced_project/src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  // destination is used to determine within which folder the uploaded files should be stored.
+  destination: function (req, file, cb) {
+    cb(null, "../../public/temp/");
+  },
+
+  // filename is used to determine what the file should be named inside the folder.
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
