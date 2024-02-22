6273b168e63a48df4483e1f4231d5d8c2db2ff9c
mukulpadwal <mukulpadwal786@gmail.com>
2024-02-22 22:58:13 +0530

    ADD : steps-for-registering-user

--- 04_advanced_project/src/controllers/user.controller.js
+++ 04_advanced_project/src/controllers/user.controller.js
@@ -2,7 +2,13 @@ import asyncHandler from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // Logic to register user
-  res.status(201).json({ success: true, message: "User registered" });
+  // 1. Get all the data sent from the client
+  // 2. We should validate the data to maintain the integrity of the data
+  // 2.1 check if the user already exists (username or email should be unique)
+  // 3. Handle the file upload
+  // 4. Create a new user object
+  // 5. Save the data to the database
+  // 6. remove sensitive data from the user object response
 });
 
 export { registerUser };
